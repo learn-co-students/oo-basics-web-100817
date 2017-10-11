e02a549ec4ce5b58f26c6e66f3da010b25f9a739
github username <github email address>
2017-10-11 14:53:00 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
 
 
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  attr_accessor :page_count, :genre
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
